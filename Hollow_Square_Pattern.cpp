73e35949777cbb17a7e6d6c409c10f63592aafc7
Aiza Farooq <72245945+aiza411@users.noreply.github.com>
2024-02-05 21:45:23 +0500

    Done

---
+++ Hollow_Square_Pattern.cpp
@@ -0,0 +1,48 @@
+/*Write a program to print holllow square pattern with stars
+
+*****
+*   *
+*   *
+*   *
+*****
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+
+    cout<<"ENTER THE SIZE";
+    cin>>size;
+
+    while(cin.fail())
+    {
+        cout<<"\n\t\tYOU HAVE NOT ENTERED A DIGIT!!!!!!";
+        cout<<"\n\t\tENTER THE SIZE AGAIN";
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin>>size;
+    }
+
+    for(int i=0 ; i<size ; i++)
+    {
+        for(int j=0 ; j<size; j++)
+        {
+            if(i==0 || i==size-1 || j==0 || j==size-1)
+            {
+                cout<<"*";
+            }
+
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
---
+++ Left_Triangle_Pattern.cpp
@@ -0,0 +1,42 @@
+/*Write a program to print Left triangle pattern with stars
+
+
+*
+**
+***
+****
+*****
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+
+    cout<<"ENTER THE SIZE";
+    cin>>size;
+
+    while(cin.fail())
+    {
+        cout<<"\n\t\tYOU HAVE NOT ENTERED A DIGIT!!!!!!";
+        cout<<"\n\t\tENTER THE SIZE AGAIN";
+        cin.clear();
+        cin.ignore(256, '\n');
+        cin>>size;
+    }
+
+    for(int i=0 ; i<size ; i++)
+    {
+        for(int j=0 ; j<=i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
